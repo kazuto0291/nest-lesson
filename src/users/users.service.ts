145c8d0cec8ea7917a9205f66f7ccecae5ebdbae
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-16 21:21:16 +0900

    userModelを取得する

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './Interfaces/user.interface';
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
   users: CreateUserDto[] = [];
   create(user: CreateUserDto) {
     this.users.push(user);
