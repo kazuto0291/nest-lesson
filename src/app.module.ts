c49a3a6a5ee193a07e6a734cd9b64a366f3f673c
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-16 20:37:17 +0900

    app.momule.-mongoose-db-conection
    データベースにコネクション設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule], //app.moduleでimportしないといけない
+  imports: [UsersModule, MongooseModule.forRoot('mongodb+srv://zuto:password@cluster0.nqlus.mongodb.net/nest?retryWrites=true&w=majority')], //app.moduleでimportしないといけない
   controllers: [AppController],
   providers: [AppService],
 })
