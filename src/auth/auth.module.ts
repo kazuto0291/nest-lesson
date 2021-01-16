d38fd274eaaf167b02789f4b14c53fe1decb2e0c
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-16 22:36:05 +0900

    ディペンデンシィインジェクションを働かせるために一回モジュール登録する
    @nestjs系を使うとき、一回モジュールに登録する。

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '1h' }, //一時間のみ有効
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
