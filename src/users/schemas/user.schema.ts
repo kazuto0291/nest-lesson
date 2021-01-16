ddcd79bfc4bc3be90e8d047c8c2432e171e66ee0
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-16 20:45:31 +0900

    dbのモデルを取得するための設定-mongooseの型定義ファイルのinstall

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,1 @@
+export const UserSchema = new mongoose
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: 'User', schema:}])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService], //providersの中のものしかかけない
