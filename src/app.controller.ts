20fc8c0674d27c25076aec43ad00917a956164c4
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-16 11:30:27 +0900

    servieceの役割はコントローラの処理の部分を担う
    コントローラがどんなURLで値を返すまでの処理をservieceに書く。
    コントローラの記述をへらすため。

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,10 +9,15 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello(); //thisはconstructorに定義されているから使える
   }
   @Get('hello/goodby')
   getHello2() {
     return 'hello';
   }
 }
+// new AppController(new AppService())
+// thisが使える条件２つ
+// app.serviceに@Injectable()というデコレーター書く
+// app.moduleでprovidersにAppServiceを登録する
+// 事によってconstructor()関数にAppService型を入れると自動的にNest.jsはAppServiceを認識してAppServiceを定義することができる
--- src/app.module.ts
+++ src/app.module.ts
@@ -10,3 +10,12 @@ import { AppService } from './app.service';
 export class AppModule {}
 
 // @Module-デコレーター（関数）--classを受け取ってデコレーションする関数（色々いじる）
+
+// Nest.jsの裏側の処理
+// new AppModule();
+// const appService = new AppService();
+// new AppController(appService);
+
+// newを書かずに外部から入れる仕込みをDI(ディペンデンシィインジェクション)−デザインパターン
+// ディペンデンシィ-依存性 インジェクション−外部から注入する−
+// コードが見やすくなったり、テストしやすくなる
