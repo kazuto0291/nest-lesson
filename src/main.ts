b2daf6ae804356f6ecb0f59d344e0786f99d4873
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-16 10:49:17 +0900

    nest-main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // app が作成したインスタンスになっている（NestFactoryというクラスがあり、クラスのstaticメソッドのcreateが作られAppMoculeが投入されている）
   const app = await NestFactory.create(AppModule);
+  // app = express()内部的になっている
   await app.listen(3000);
 }
 bootstrap();
