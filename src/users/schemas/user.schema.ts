d83bc690a48988bb4b00ab07a86ef2fa72b3981f
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-16 20:55:59 +0900

    Schemaの設定

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,1 +1,13 @@
-export const UserSchema = new mongoose
+import * as mongoose from 'mongoose';
+export const UserSchema = new mongoose.Schema({
+  username: {
+    // 同じ名前のusernameは存在できない
+    type: String,
+    required: true,
+    index: { unique: true },
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
