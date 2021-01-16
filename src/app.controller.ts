fbc59b98387549c3e262f42b4216bfc9d1af0efb
kazuto0291 <zutozuto03170291@gmail.com>
2021-01-16 11:07:58 +0900

    コントローラ
    コントローラの役割はどんなURL来たらどんな値を返すか書かれている。
    
    GET()の中にしていするURLを書きネストを深くできる。
    ControllerにもURLを書くことができる。

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+// コントローラの役割--どんなURL来たらどんな値を返すかが書かれている。
+// サーバーとしてコントローラがメイン
+@Controller('base')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('hello/goodby')
+  getHello2() {
+    return 'hello';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -8,3 +8,5 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+
+// @Module-デコレーター（関数）--classを受け取ってデコレーションする関数（色々いじる）
